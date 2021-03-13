c17e71bc0a76f9e87858bc8d367cb87c9a7e9506
pritam <pritam@gmail.com>
2021-03-13 09:41:47 +0530

    Line added to check the function call

--- src/com/art/controller/Mail.java
+++ src/com/art/controller/Mail.java
@@ -21,6 +21,7 @@ public class Mail {
 		String from="sjartandcreation@gmail.com";
 		
 		sendMail(to,subject,message,from);
+		System.out.println("Message function called..");
 	}
 	
 	public static void sendMail(String to,String subject,String message,String from) {
